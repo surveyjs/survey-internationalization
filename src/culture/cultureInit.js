72e53613e696ba27ea82c231a5e0ad53ff95d9ff
gologames <gologames@users.noreply.github.com>
2019-12-09 18:48:20 +0300

    Add short date format default value

--- src/culture/cultureInit.js
+++ src/culture/cultureInit.js
@@ -9,6 +9,11 @@ export function initCulture(Survey) {
             // eslint-disable-next-line
             question.survey && question.survey.culture || ""); 
     }
+    // function getCultureProperty(question, propertyName) {
+    //     return question.getPropertyValue(propertyName,
+    //         Survey.cultureInfo.getCulture(
+    //             getQuestionCulture(question))[propertyName + "s"][0].value); 
+    // }
     
     Survey.Serializer.addProperty("survey", {
         name: "culture",
@@ -40,26 +45,39 @@ export function initCulture(Survey) {
                 .map((value) => {
                     return {
                         text: (value ? "" : "Default: ") +
-                            Survey.cultureInfo.getCulture(value).name,
+                            Survey.cultureInfo.getCulture(
+                                // eslint-disable-next-line
+                                value ||
+                                // eslint-disable-next-line
+                                obj.survey && obj.survey.culture || "").name,
                         value: value
-                    }
+                    };
                 });
         },
-        onGetValue: getQuestionCulture,
         default: ""
     });
     Survey.Serializer.addProperty("text", {
         name: "shortDateFormat",
         dependsOn: "culture",
         choices: function(obj) {
-            return [""].concat(Survey.cultureInfo
-                .getCulture(getQuestionCulture(obj)).shortDateFormats);
+            var formats = Survey.cultureInfo.getCulture(
+                getQuestionCulture(obj)).shortDateFormats;
+            var defaultFormat = {
+                text: "Default: " + formats[0].text,
+                value: ""        
+            }
+            return [defaultFormat].concat(formats);
+            //     return [{
+            //     text: "Default: " + getCultureProperty(obj, "shortDateFormat"),
+            //     value: ""
+            // }].concat(Survey.cultureInfo
+            //     .getCulture(getQuestionCulture(obj)).shortDateFormats);
         },
-        onGetValue: function(obj) {
-            return obj.getPropertyValue("shortDateFormat",
-                Survey.cultureInfo.getCulture(
-                    getQuestionCulture(obj)).shortDateFormats[0].value); 
-        },
+        // onGetValue: function(obj) {
+        //     return obj.getPropertyValue("shortDateFormat",
+        //         Survey.cultureInfo.getCulture(
+        //             getQuestionCulture(obj)).shortDateFormats[0].value); 
+        // },
         default: ""
     });
 };
